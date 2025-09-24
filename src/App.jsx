c0a646eeb385c64cb73d391b02535630677ddc7b
artem <jock.fromrussia@gmail.com>
2025-09-24 13:40:06 +0300

    added raw structure in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { useState } from 'react';
+import api from './api/axios';
+
 const App = () => {
-	return <div>App</div>;
+	const [users, setUsers] = useState([]);
+
+	const getUsersAsync = async () => {
+		try {
+			const res = await api.get('/users');
+			setUsers(res.data);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
+	const setToken = () => {
+		localStorage.setItem('authToken', 'firstToken');
+	};
+
+	return (
+		<>
+			<button onClick={getUsersAsync}>getUsers</button>
+			<button onClick={setToken}>setToken</button>
+
+			<h5>пользователи</h5>
+
+			<ul>
+				{users.map((u) => (
+					<li key={u.name}>
+						<p>
+							name: {u.name} age: {u.age}
+						</p>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 };
 
 export default App;
